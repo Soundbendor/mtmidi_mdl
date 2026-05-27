90aa1c04fc3b529e3e2eb2443a5231dcbd4a9f6b
Derek Kwan <dkwan6@gatech.edu>
2026-05-26 17:36:02 -0700

    update mover

--- misc/hf_move.py
+++ misc/hf_move.py
@@ -5,8 +5,9 @@ from util import util_constants as UC
 import sys
 
 mv_test = True
-datdir = '/nfs/guille/eecs_research/soundbendor/kwand/mtmidi_prb/acts'
-dataset = 'secondary_dominants'
+#datdir = '/nfs/guille/eecs_research/soundbendor/kwand/mtmidi_mdl/acts'
+datdir = '/nfs/hpc/share/kwand/mtmidi_mdl/acts/'
+datasets = ['notes', 'scales', 'intervals', 'simple_progressions', 'tempos', 'chords', 'time_signatures']
 if len(sys.argv) > 1:
     dataset = sys.argv[1]
 
@@ -19,20 +20,22 @@ df = pl.read_csv(csvfile)
 
 #for model_size in UC.MODEL_NUM_LAYERS.keys():
 #for model_size in ['musicgen-large']:
-for model_size in ['baseline-concat', 'baseline-chroma', 'baseline-mfcc', 'baseline-mel', 'musicgen-audio', 'musicgen-small', 'musicgen-medium', 'musicgen-large', 'jukebox']:
-    for i in range(len(df)):
-        cur_fp = os.path.join(dsdatdir, model_size)
-        cur_name = df[i]['name'][0]
-        cur_dat = f'{cur_name}.dat'
-        from_fp = os.path.join(cur_fp, cur_dat)
-        cur_fold = df[i]['fold'][0]
-        fold_folder = f'fold_{cur_fold}'
-        fold_fp = os.path.join(cur_fp, fold_folder)
-        to_fp = os.path.join(fold_fp, cur_dat)
-        if os.path.isdir(fold_fp) == False:
-            os.mkdir(fold_fp)
-        #print(f'moving from {from_fp} to {to_fp}')
-        os.rename(from_fp, to_fp)
+for dataset in datasets:
+    for model_size in ["MERT-v1-95M", "MERT-v1-330M", "wav2vec2-base", "wav2vec2-large",'musicgen-small', 'musicgen-medium', 'musicgen-large']:
+
+        for i in range(len(df)):
+            cur_fp = os.path.join(dsdatdir, model_size)
+            cur_name = df[i]['name'][0]
+            cur_dat = f'{cur_name}.dat'
+            from_fp = os.path.join(cur_fp, cur_dat)
+            cur_fold = df[i]['fold'][0]
+            fold_folder = f'fold_{cur_fold}'
+            fold_fp = os.path.join(cur_fp, fold_folder)
+            to_fp = os.path.join(fold_fp, cur_dat)
+            if os.path.isdir(fold_fp) == False:
+                os.mkdir(fold_fp)
+            #print(f'moving from {from_fp} to {to_fp}')
+            os.rename(from_fp, to_fp)
 
 
 
