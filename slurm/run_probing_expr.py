85957ba63ea54f0b8ec1ce0adccf7988aceb11a4
Derek Kwan <dkwan6@gatech.edu>
2026-06-08 10:28:05 -0700

    fix

--- slurm/run_probing_expr.py
+++ slurm/run_probing_expr.py
@@ -52,18 +52,19 @@ if __name__ == "__main__":
 
 
     start_time = str(int(time.time() * 1000))
-
+    
+    using_part_rto = args.unbiased_part_rto == True or args.biased_part_rto == True: 
     expr_short = UC.EXPR_SHORT[args.expr_type] 
     for dataset in args.datasets:
         ds_short = UC.DATASET_SHORT[dataset]
         for model_size in args.model_sizes:
             size_short = UC.MODEL_SIZES_SHORT[model_size]         
             job_str = f'{expr_short}-{ds_short}-{size_short}'
-            if args.stats == True and args.part_rto == False:
+            if args.stats == True and using_part_rto == False:
                 job_str = f'sts_{job_str}'
-            if args.stats == False and args.part_rto == True:
+            if args.stats == False and using_part_rto == True:
                 job_str = f'pto_{job_str}'
-            if args.stats == False and args.part_rto == False:
+            if args.stats == False and using_part_rto == False:
                 job_str = f'mdl_{job_str}'
             slurm_strarr1 = ["#!/bin/bash"]
             slurm_strarr2 = [f"#SBATCH -p {args.partition}"]
@@ -74,13 +75,13 @@ if __name__ == "__main__":
                     slurm_strarr2 = ['#SBATCH -A soundbendor', f"#SBATCH -p {args.partition}"]
             slurm_strarr3 = [f"#SBATCH --mem={args.ram_mem}G", f"#SBATCH --gres=gpu:{args.gpus}", f"#SBATCH -t {args.num_days}-00:00:00", f"#SBATCH --job-name={job_str}", "#SBATCH --export=ALL", f"#SBATCH --output=/nfs/guille/eecs_research/soundbendor/kwand/out_mtmidi_mdl/{job_str}-%j.out", ""]
             slurm_strarr = slurm_strarr1 + slurm_strarr2 + slurm_strarr3
-            p_str = f"python {py_path}  -st {args.stats} -pr {args.part_rto} -ds {dataset} -et {args.expr_type} -ms {model_size} -sh {args.from_share} -wdb {args.use_wandb} -cd {args.use_cuda} -sf {args.suffix}" 
+            p_str = f"python {py_path}  -st {args.stats} -upr {args.unbiased_part_rto}  -bpr {args.biased_part_rto} -nstd {args.nonstandard} -ds {dataset} -et {args.expr_type} -ms {model_size} -sh {args.from_share} -wdb {args.use_wandb} -cd {args.use_cuda} -sf {args.suffix}" 
             slurm_strarr.append(p_str)
             script_fname = f"{start_time}_{job_str}.sh"
             script_path = os.path.join(sh_dir, script_fname)
             script_str = "\n".join(slurm_strarr)
             script_idx += 1
-            print(f"===== {args.expr_type} | {dataset} | {model_size} | STATS: {args.stats} | PR: {args.part_rto} =====")
+            print(f"===== {args.expr_type} | {dataset} | {model_size} | STATS: {args.stats} | UPR: {args.unbiased_part_rto} | BPR: {args.biased_part_rto}=====")
             print(f"Creating {script_fname}")
             with open(script_path, 'w') as f:
                 f.write(script_str)
