8b5978751bca8cf5fd1605e284ae2839737d9076
derekxkwan <derek.x.kwan@gmail.com>
2026-06-08 23:52:58 -0700

    update

--- util/util_wandb.py
+++ util/util_wandb.py
@@ -61,7 +61,7 @@ def init(wdict, override = None):
 
 
 def build_initdict(parser_args, _config):
-    _d = {'entity': entity, 'project': f'onlinemdl_preq-{parser_args.expr_type}', 'dir': UC.WANDB_PATH, 'settings': wandb.Settings(init_timeout=120)}
+    _d = {'entity': entity, 'project': f'online-mt_mdl-{parser_args.expr_type}', 'dir': UC.WANDB_PATH, 'settings': wandb.Settings(init_timeout=120)}
     _d['config'] = _config
     return _d
 
