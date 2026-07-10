99072b021ea7bbb2b27c71229518fa6c0cf82cd8
Derek Kwan <dkwan6@gatech.edu>
2026-07-10 16:15:09 -0700

    make faster

--- two_nn.py
+++ two_nn.py
@@ -1,69 +1,27 @@
 import torch
 
-def get_mu_i_old(pt1, other_pts, batch_size=64):
-    ret = torch.zeros(2, dtype=pt1.dtype)
-    ret[0] = torch.inf
-    ret[1] = torch.inf
-    num_other = other_pts.shape[0]
-    cur_split  = 0
-    last_split = 0
-    comp = None
-    if len(pt1.shape) < 2:
-        comp = pt1.unsqueeze(0)
-    else:
-        comp = pt1
-    
-    step_size = batch_size
-    if batch_size < 0:
-        step_size = num_other
-    while last_split < num_other:
-        cur_split = last_split + step_size
-        len_split = other_pts[last_split:cur_split].shape[0]
-        cur_dists = torch.cdist(comp, other_pts[last_split:cur_split], p=2)
-        for i in range(len_split):
-            cur_dist = cur_dists[0][i]
-            if cur_dist < ret[1]:
-                is_smallest = cur_dist < ret[0]
-                if is_smallest == True:
-                    ret[1] = ret[0]
-                    ret[0] = cur_dist
-                else:
-                    ret[1] = cur_dist
-        last_split = cur_split
-    mu = 0.
-    if ret[0] > 0.:
-        mu = (ret[1]/ret[0])
-    return mu
-
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.pdist.html
 def get_dist_idx(_i,_j, N):
     i = min(_i,_j)
     j = max(_i,_j)
     return N * i + j - ((i+2) * (i+1)) // 2
 
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.pdist.html
+def get_dist_idxs(cur_idx, N):
+    idxs = torch.arange(N)
+    ipts = torch.vstack((torch.ones(N-1, dtype=torch.int64) * cur_idx, torch.hstack((idxs[:cur_idx], idxs[(cur_idx+1):])))).sort(axis=0,descending=False).values
+    ret = N * ipts[0] + ipts[1] - ((ipts[0]+2) * (ipts[0]+1)) // 2
+    return ret
+
+
 def get_mu_i(cur_idx, N, dist_vec,device='cpu'):
-    ret = torch.zeros(2, dtype=dist_vec.dtype, device=device)
-    ret[0] = torch.inf
-    ret[1] = torch.inf
-    for other_idx in range(N):
-        if other_idx != cur_idx:
-            dist_idx = get_dist_idx(cur_idx, other_idx, N)
-            cur_dist = dist_vec[dist_idx]
-            if cur_dist < ret[1]:
-                is_smallest = cur_dist < ret[0]
-                if is_smallest == True:
-                    ret[1] = ret[0]
-                    ret[0] = cur_dist
-                else:
-                    ret[1] = cur_dist
+    #log_mus = torch.log(mus.index_select(0, idxs[idxs != max_idx])).unsqueeze(1)
+    idxs = get_dist_idxs(cur_idx, N)
+    cur_dists = dist_vec[idxs]
+    ret = cur_dists.sort(axis=0).values[:2]
     mu = 0.
     if ret[0] > 0.:
         mu = (ret[1]/ret[0])
     return mu
-
-
-
-
 
 def calc_twonn(datapts, batch_size = 64, unused_pct = 0.0, device='cpu'):
     batch_dim = 0
@@ -103,11 +61,4 @@ def calc_twonn(datapts, batch_size = 64, unused_pct = 0.0, device='cpu'):
     # least squares solution
     slope = xty/xtx
     return slope
-
-
-
-
-
-
-
 
